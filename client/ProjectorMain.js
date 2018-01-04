68fbd2caae13265fb569be183135af8a22529d42
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-01-03 17:55:48 -0800

    Allow getting a supporter canvas

--- client/ProjectorMain.js
+++ client/ProjectorMain.js
@@ -24,6 +24,11 @@ const canvasSizeMatrix = forwardProjectionMatrixForPoints([
 ]).adjugate();
 
 class Program extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { showCanvas: false, showSupportCanvas: false };
+  }
+
   componentDidMount() {
     this._worker = new Worker(this.props.program.currentCodeUrl);
     this._worker.onmessage = this._receiveMessage;
@@ -40,10 +45,31 @@ class Program extends React.Component {
       if (sendData.name === 'number') {
         this._worker.postMessage({ messageId, receiveData: { object: this.props.program.number } });
       } else if (sendData.name === 'canvas') {
-        this._offscreen = this._offscreen || this._canvas.transferControlToOffscreen();
-        this._worker.postMessage({ messageId, receiveData: { object: this._offscreen } }, [
-          this._offscreen,
-        ]);
+        if (this.state.showCanvas) {
+          this._worker.postMessage({ messageId, receiveData: { object: null } });
+        } else {
+          this._canvasAvailableCallback = canvas => {
+            const offscreen = canvas.transferControlToOffscreen();
+            this._worker.postMessage({ messageId, receiveData: { object: offscreen } }, [
+              offscreen,
+            ]);
+            delete this._canvasAvailableCallback;
+          };
+          this.setState({ showCanvas: true });
+        }
+      } else if (sendData.name === 'supporterCanvas') {
+        if (this.state.showSupporterCanvas) {
+          this._worker.postMessage({ messageId, receiveData: { object: null } });
+        } else {
+          this._supporterCanvasAvailableCallback = canvas => {
+            const offscreen = canvas.transferControlToOffscreen();
+            this._worker.postMessage({ messageId, receiveData: { object: offscreen } }, [
+              offscreen,
+            ]);
+            delete this._supporterCanvasAvailableCallback;
+          };
+          this.setState({ showSupporterCanvas: true });
+        }
       }
     }
   };
@@ -57,23 +83,50 @@ class Program extends React.Component {
     ).multiply(canvasSizeMatrix);
 
     return (
-      <canvas
-        key={program.id}
-        ref={el => (this._canvas = el)}
-        width={canvasWidth}
-        height={canvasHeight}
-        style={{
-          position: 'absolute',
-          left: 0,
-          top: 0,
-          width: canvasWidth,
-          height: canvasHeight,
-          transform: matrixToCssTransform(matrix),
-          transformOrigin: '0 0 0',
-          boxSizing: 'border-box',
-          border: '10px solid black',
-        }}
-      />
+      <div>
+        {this.state.showCanvas && (
+          <canvas
+            key="canvas"
+            ref={el => {
+              if (el && this._canvasAvailableCallback) {
+                this._canvasAvailableCallback(el);
+              }
+            }}
+            width={canvasWidth}
+            height={canvasHeight}
+            style={{
+              position: 'absolute',
+              left: 0,
+              top: 0,
+              width: canvasWidth,
+              height: canvasHeight,
+              transform: matrixToCssTransform(matrix),
+              transformOrigin: '0 0 0',
+              boxSizing: 'border-box',
+              border: '10px solid black',
+            }}
+          />
+        )}
+        {this.state.showSupporterCanvas && (
+          <canvas
+            key="supporterCanvas"
+            ref={el => {
+              if (el && this._supporterCanvasAvailableCallback) {
+                this._supporterCanvasAvailableCallback(el);
+              }
+            }}
+            width={width}
+            height={height}
+            style={{
+              position: 'absolute',
+              left: 0,
+              top: 0,
+              width: width,
+              height: height,
+            }}
+          />
+        )}
+      </div>
     );
   }
 }
