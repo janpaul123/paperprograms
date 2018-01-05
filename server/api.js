33ed49280a6d56c3ae8bbc20a00bd23989543f9a
Jan Paul Posma <janpaul123@users.noreply.github.com>
2018-01-05 10:56:37 -0800

    No caching of requests

--- server/api.js
+++ server/api.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const router = express.Router();
 router.use(express.json());
+router.use(require('nocache')());
 
 const knex = require('knex')(require('../knexfile')[process.env.NODE_ENV || 'development']);
 
