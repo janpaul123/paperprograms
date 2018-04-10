7559614e4ccf52cba20c59bb45f2904f679d1890
Paul Sonnentag <paul.sonnentag@googlemail.com>
2018-04-10 09:16:51 +0200

    Fix requesting max video resolution

--- client/camera/CameraVideo.js
+++ client/camera/CameraVideo.js
@@ -19,9 +19,19 @@ export default class CameraVideo extends React.Component {
       navigator.mediaDevices
         .getUserMedia({
           audio: false,
+
+          // specifying "ideal" property with high value doesn't work with some webcams in Chrome
+          // instead pick maximum from range of resolutions
+          // based on: https://stackoverflow.com/questions/27420581/get-maximum-video-resolution-with-getusermedia#answer-27444179
           video: {
-            width: { ideal: 100000 },
-            height: { ideal: 100000 },
+            optional: [
+              {minWidth: 320},
+              {minWidth: 640},
+              {minWidth: 1024},
+              {minWidth: 1280},
+              {minWidth: 1920},
+              {minWidth: 2560},
+            ]
           },
         })
         .then(stream => {
