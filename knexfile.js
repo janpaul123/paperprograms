d409fc80ee7c1924d8e95af56b7dfbf838b8a5c0
Paul Sonnentag <paul.sonnentag@googlemail.com>
2018-05-14 14:16:37 +0200

    Make db for development and docker configurable with env variable

--- knexfile.js
+++ knexfile.js
@@ -3,11 +3,11 @@ require('dotenv').config();
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/paper_programs_development',
+    connection: process.env.DATABASE_URL || 'postgres://localhost/paper_programs_development',
   },
   docker: {
     client: 'pg',
-    connection: 'postgres://root@postgres/paper_programs_development',
+    connection: process.env.DATABASE_URL || 'postgres://root@postgres/paper_programs_development',
   },
   production: {
     client: 'pg',
