88750e16f9b306cd726b7142fb83c1cd5ec9406b
Mark Kvetny <m.kvetny@outlook.com>
2018-10-19 16:19:47 +0200

    changed labeller defaults

--- client/projector/core/labeller.js
+++ client/projector/core/labeller.js
@@ -5,10 +5,10 @@ const { Illumination } = window;
 module.exports = function() {
   When`{someone} wishes {paper} is labelled {text}`(({ paper, text }) => {
     const ill = new Illumination().text({
-      x: 0,
-      y: 30,
+      x: 10,
+      y: 35,
       text,
-      fill: 'dodgerblue',
+      fill: 'crimson',
       font: '30px sans-serif',
     });
     Wish`${paper} has illumination ${ill}`;
