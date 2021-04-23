4c17c1221f366b83c73a874d1f3dfc710052fdfb
JP Posma <janpaul123@users.noreply.github.com>
2021-04-22 16:32:52 -1000

    Move pages to root and add CNAME file

--- .importjs.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  aliases: {
-    styles: './{filename}.css',
-  },
-};
--- server/entry-server.js
+++
@@ -1,7 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  require('./main');
-} else {
-  const nodemon = require('nodemon');
-  const path = require('path');
-  nodemon({ script: path.join(__dirname, 'main.js'), watch: __dirname });
-}
