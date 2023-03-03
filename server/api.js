91e5f93d813c50a94a46dc50b99d5ab5e0b6e7b9
Jesse <jesse.greenberg@colorado.edu>
2023-03-03 14:05:58 -0500

    format to phet style, see #47

--- server/api.js
+++ server/api.js
@@ -1,41 +1,41 @@
-const express = require('express');
-const crypto = require('crypto');
+const express = require( 'express' );
+const crypto = require( 'crypto' );
 
 const editorHandleDuration = 1500;
 
 const router = express.Router();
-router.use(express.json());
-router.use(require('nocache')());
+router.use( express.json() );
+router.use( require( 'nocache' )() );
 
-const knex = require('knex')(require('../knexfile')[process.env.NODE_ENV || 'development']);
+const knex = require( 'knex' )( require( '../knexfile' )[ process.env.NODE_ENV || 'development' ] );
 
-router.get('/program.:spaceName.:number.js', (req, res) => {
+router.get( '/program.:spaceName.:number.js', ( req, res ) => {
   const { spaceName, number } = req.params;
   knex
-    .select('currentCode')
-    .from('programs')
-    .where({ spaceName, number })
-    .then(selectResult => {
-      res.set('Content-Type', 'text/javascript;charset=UTF-8');
-      res.send(selectResult[0].currentCode);
-    });
-});
+    .select( 'currentCode' )
+    .from( 'programs' )
+    .where( { spaceName, number } )
+    .then( selectResult => {
+      res.set( 'Content-Type', 'text/javascript;charset=UTF-8' );
+      res.send( selectResult[ 0 ].currentCode );
+    } );
+} );
 
 // Get a list of all the spaces available in the DB.
-router.get('/api/spaces-list', (req, res) => {
+router.get( '/api/spaces-list', ( req, res ) => {
   knex
     .distinct()
-    .from('programs')
-    .pluck('spaceName')
-    .then(spaceNames => {
-      res.json(spaceNames);
-    });
-});
+    .from( 'programs' )
+    .pluck( 'spaceName' )
+    .then( spaceNames => {
+      res.json( spaceNames );
+    } );
+} );
 
 // Add a new space to the DB.
-router.get('/api/add-space/:newSpaceName', (req, res) => {
+router.get( '/api/add-space/:newSpaceName', ( req, res ) => {
   console.log( `req.params.newSpaceName = ${req.params.newSpaceName}` );
-  res.json(req.params);
+  res.json( req.params );
   // knex
   //   .distinct()
   //   .from('programs')
@@ -43,143 +43,156 @@ router.get('/api/add-space/:newSpaceName', (req, res) => {
   //   .then(spaceNames => {
   //     res.json(spaceNames);
   //   });
-});
+} );
 
-function getSpaceData(req, callback) {
+function getSpaceData( req, callback ) {
   const { spaceName } = req.params;
-  knex('programs')
-    .select('number', 'originalCode', 'currentCode', 'printed', 'editorInfo')
-    .where({ spaceName })
-    .then(programData => {
-      callback({
-        programs: programData.map(program => {
-          const editorInfo = JSON.parse(program.editorInfo || '{}');
+  knex( 'programs' )
+    .select( 'number', 'originalCode', 'currentCode', 'printed', 'editorInfo' )
+    .where( { spaceName } )
+    .then( programData => {
+      callback( {
+        programs: programData.map( program => {
+          const editorInfo = JSON.parse( program.editorInfo || '{}' );
 
           return {
             ...program,
             currentCodeUrl: `program.${spaceName}.${program.number}.js`,
             currentCodeHash: crypto
-              .createHmac('sha256', '')
-              .update(program.currentCode)
-              .digest('hex'),
+              .createHmac( 'sha256', '' )
+              .update( program.currentCode )
+              .digest( 'hex' ),
             debugUrl: `/api/spaces/${spaceName}/programs/${program.number}/debugInfo`,
             claimUrl: `/api/spaces/${spaceName}/programs/${program.number}/claim`,
             editorInfo: {
               ...editorInfo,
-              claimed: !!(editorInfo.time && editorInfo.time + editorHandleDuration > Date.now()),
+              claimed: !!( editorInfo.time && editorInfo.time + editorHandleDuration > Date.now() ),
             },
             codeHasChanged: program.currentCode !== program.originalCode,
           };
-        }),
+        } ),
         spaceName,
-      });
-    });
+      } );
+    } );
 }
 
-router.get('/api/spaces/:spaceName', (req, res) => {
-  getSpaceData(req, spaceData => {
-    res.json(spaceData);
-  });
-});
+router.get( '/api/spaces/:spaceName', ( req, res ) => {
+  getSpaceData( req, spaceData => {
+    res.json( spaceData );
+  } );
+} );
 
 const maxNumber = 8400 / 4;
-router.post('/api/spaces/:spaceName/programs', (req, res) => {
+router.post( '/api/spaces/:spaceName/programs', ( req, res ) => {
   const { spaceName } = req.params;
   const { code } = req.body;
-  if (!code) return res.status(400).send('Missing "code"');
+  if ( !code ) {
+    return res.status( 400 ).send( 'Missing "code"' );
+  }
 
   knex
-    .select('number')
-    .from('programs')
-    .where({ spaceName })
-    .then(selectResult => {
-      const existingNumbers = selectResult.map(result => result.number);
+    .select( 'number' )
+    .from( 'programs' )
+    .where( { spaceName } )
+    .then( selectResult => {
+      const existingNumbers = selectResult.map( result => result.number );
       const potentialNumbers = [];
-      for (let i = 0; i < maxNumber; i++) {
-        if (!existingNumbers.includes(i)) potentialNumbers.push(i);
+      for ( let i = 0; i < maxNumber; i++ ) {
+        if ( !existingNumbers.includes( i ) ) {
+          potentialNumbers.push( i );
+        }
       }
-      if (potentialNumbers.length === 0) return res.status(400).send('No more available numbers');
-      const number = potentialNumbers[Math.floor(Math.random() * potentialNumbers.length)];
+      if ( potentialNumbers.length === 0 ) {
+        return res.status( 400 ).send( 'No more available numbers' );
+      }
+      const number = potentialNumbers[ Math.floor( Math.random() * potentialNumbers.length ) ];
 
-      knex('programs')
-        .insert({ spaceName, number, originalCode: code, currentCode: code })
-        .then(() => {
-          getSpaceData(req, spaceData => {
-            res.json({ number, spaceData });
-          });
-        });
-    });
-});
+      knex( 'programs' )
+        .insert( { spaceName, number, originalCode: code, currentCode: code } )
+        .then( () => {
+          getSpaceData( req, spaceData => {
+            res.json( { number, spaceData } );
+          } );
+        } );
+    } );
+} );
 
-router.put('/api/spaces/:spaceName/programs/:number', (req, res) => {
+router.put( '/api/spaces/:spaceName/programs/:number', ( req, res ) => {
   const { spaceName, number } = req.params;
   const { code } = req.body;
-  if (!code) return res.status(400).send('Missing "code"');
+  if ( !code ) {
+    return res.status( 400 ).send( 'Missing "code"' );
+  }
 
-  knex('programs')
-    .update({ currentCode: code })
-    .where({ spaceName, number })
-    .then(() => {
-      res.json({});
-    });
-});
+  knex( 'programs' )
+    .update( { currentCode: code } )
+    .where( { spaceName, number } )
+    .then( () => {
+      res.json( {} );
+    } );
+} );
 
-router.post('/api/spaces/:spaceName/programs/:number/markPrinted', (req, res) => {
+router.post( '/api/spaces/:spaceName/programs/:number/markPrinted', ( req, res ) => {
   const { spaceName, number } = req.params;
   const { printed } = req.body;
-  if (printed === undefined) return res.status(400).send('Missing "printed"');
+  if ( printed === undefined ) {
+    return res.status( 400 ).send( 'Missing "printed"' );
+  }
 
-  knex('programs')
-    .update({ printed })
-    .where({ spaceName, number })
-    .then(() => {
-      getSpaceData(req, spaceData => {
-        res.json(spaceData);
-      });
-    });
-});
+  knex( 'programs' )
+    .update( { printed } )
+    .where( { spaceName, number } )
+    .then( () => {
+      getSpaceData( req, spaceData => {
+        res.json( spaceData );
+      } );
+    } );
+} );
 
-router.put('/api/spaces/:spaceName/programs/:number/debugInfo', (req, res) => {
+router.put( '/api/spaces/:spaceName/programs/:number/debugInfo', ( req, res ) => {
   const { spaceName, number } = req.params;
 
-  knex('programs')
-    .update({ debugInfo: JSON.stringify(req.body) })
-    .where({ spaceName, number })
-    .then(() => {
-      res.json({});
-    });
-});
+  knex( 'programs' )
+    .update( { debugInfo: JSON.stringify( req.body ) } )
+    .where( { spaceName, number } )
+    .then( () => {
+      res.json( {} );
+    } );
+} );
 
-router.post('/api/spaces/:spaceName/programs/:number/claim', (req, res) => {
+router.post( '/api/spaces/:spaceName/programs/:number/claim', ( req, res ) => {
   const { spaceName, number } = req.params;
 
   knex
-    .select(['debugInfo', 'editorInfo'])
-    .from('programs')
-    .where({ spaceName, number })
-    .then(selectResult => {
-      if (selectResult.length === 0) return res.status(404);
-      const editorInfo = JSON.parse(selectResult[0].editorInfo || '{}');
+    .select( [ 'debugInfo', 'editorInfo' ] )
+    .from( 'programs' )
+    .where( { spaceName, number } )
+    .then( selectResult => {
+      if ( selectResult.length === 0 ) {
+        return res.status( 404 );
+      }
+      const editorInfo = JSON.parse( selectResult[ 0 ].editorInfo || '{}' );
       if (
         editorInfo.time &&
         editorInfo.time + editorHandleDuration > Date.now() &&
         editorInfo.editorId !== req.body.editorId
       ) {
-        res.status(400);
-        res.json({});
+        res.status( 400 );
+        res.json( {} );
         return;
-      } else {
-        knex('programs')
-          .update({ editorInfo: JSON.stringify({ ...req.body, time: Date.now() }) })
-          .where({ spaceName, number })
-          .then(() => {
-            res.json({
-              debugInfo: JSON.parse(selectResult[0].debugInfo || '{}'),
-              editorInfo,
-            });
-          });
       }
-    });
-});
+      else {
+        knex( 'programs' )
+          .update( { editorInfo: JSON.stringify( { ...req.body, time: Date.now() } ) } )
+          .where( { spaceName, number } )
+          .then( () => {
+            res.json( {
+              debugInfo: JSON.parse( selectResult[ 0 ].debugInfo || '{}' ),
+              editorInfo,
+            } );
+          } );
+      }
+    } );
+} );
 
 module.exports = router;
